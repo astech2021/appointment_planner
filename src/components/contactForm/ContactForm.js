bd61af42d9520b87cdc48f86fc93e477bcb15f6a
ABDUL-WADUD SUMAILA <88601172+astech2021@users.noreply.github.com>
2022-01-12 02:32:11 +0000

    initial commit

---
+++ src/components/contactForm/ContactForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export const ContactForm = ({
+  name,
+  setName,
+  phone,
+  setPhone,
+  email,
+  setEmail,
+  handleSubmit
+}) => {
+  return (
+    <div className='centered'>
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          placeholder="Full Name"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="Phone Number"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="Email Address"
+        />
+      </label>
+      <br />
+      <input type="submit" value="Add Contact" />
+    </form>
+    </div>
+  );
+};
